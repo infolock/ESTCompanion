15e312641800260033e9ce8fcda59ab899cac7c9
Jonathon Hibbard <jhibbard@gmail.com>
2014-05-24 01:11:30 -0400

    Initial Import

---
+++ ESTCompanion/Categories/ESTBeaconManager+UUIDGroup.h
@@ -0,0 +1,76 @@
+//
+//  ESTBeaconManager+UUIDGroup.h
+//  estimoteTraining
+//
+//  Created by Jonathon Hibbard on 5/23/14.
+//  Copyright (c) 2014 DataOCD. All rights reserved.
+//
+
+#import "ESTBeaconManager.h"
+
+/**
+ * The callee must be this type of delegate if they wish to use the Start Ranging Immediately init option...
+ */
+@protocol ESTBeaconManagerUUIDGroupDelegate <ESTBeaconManagerDelegate>
+
+@optional
+-(NSArray *)ESTBeaconManagerDelegateShouldReturnUUIDIdentifierArray;
+@end
+
+/**
+ * Specify how any ranged beacons should be grouped in the array.  The default, kUUIDGroupByNone, just returns the array
+ * how Estimote does already...
+ */
+typedef NS_ENUM(NSInteger, kUUIDGroupBy) {
+    kUUIDGroupByNone = 0,        // Default ( 2D array containing only beacons with a specific UUID )
+    kUUIDGroupByMajor = 1,       // Group all by the MAJOR value
+    kUUIDGroupByCLProximity = 2  // Group all by CLProximity ( keys will be: immediate, near, far, unknown )
+};
+
+/**
+ * Specify the order by which ranged beacons will appear in the array
+ */
+typedef NS_ENUM(NSUInteger, kUUIDGroupSortBy) {
+    kUUIDGroupSortByDefault = 0,      // Default ( Sort by Proximity )
+    kUUIDGroupSortByCLProximity = 0,  // Sort by Proximity ( Default )
+    kUUIDGroupSortByMajorMinor = 1    // Sort by Major Value with the Minor Value being used for siblings having the same Major value
+};
+
+/**
+ * Options to
+ */
+typedef NS_OPTIONS(NSUInteger, kUUIDGroupInitOptions) {
+    kUUIDGroupInitOptionsNone = 0,
+    kUUIDGroupInitOptionsAvoidUnknownStateBeacons = 1 << 0,
+    kUUIDGroupInitOptionsStartRangingImmediately = 1 << 1   // NOTE: The callee must be a ESTBeaconManagerUUIDGroupDelegate in order to use this!
+};
+
+
+@interface ESTBeaconManager ( UUIDGroup )
+
+/**
+ * Initialize without any initOptions
+ */
+-(instancetype)initWithDelegate:(id <ESTBeaconManagerUUIDGroupDelegate>)delegate groupBy:(kUUIDGroupBy)groupBy sortBy:(kUUIDGroupSortBy)sortBy;
+// Companions
+-(instancetype)initWithDelegate:(id <ESTBeaconManagerUUIDGroupDelegate>)delegate;
+-(instancetype)initWithDelegate:(id <ESTBeaconManagerUUIDGroupDelegate>)delegate groupBy:(kUUIDGroupBy)groupBy;
+-(instancetype)initWithDelegate:(id <ESTBeaconManagerUUIDGroupDelegate>)delegate sortBy:(kUUIDGroupSortBy)sortBy;
+
+
+/**
+ * Initialize and start up some boiler plate stuff for free...
+ *
+ * @param kUUIDGroupInitOptions        initOptions  ( required )
+ * @param id<ESTBeaconManagerDelegate> delegate     ( optional - nil by default )
+ * @param kUUIDGroupBy                 groupBy      ( required )
+ * @param kUUIDGroupSortBy             sortBy       ( required )
+ */
+-(instancetype)initWithOptions:(kUUIDGroupInitOptions)initOptions delegate:(id <ESTBeaconManagerUUIDGroupDelegate>)delegate groupBy:(kUUIDGroupBy)groupBy sortBy:(kUUIDGroupSortBy)sortBy;
+// Companions
+-(instancetype)initWithOptions:(kUUIDGroupInitOptions)initOptions delegate:(id <ESTBeaconManagerUUIDGroupDelegate>)delegate;
+-(instancetype)initWithOptions:(kUUIDGroupInitOptions)initOptions delegate:(id <ESTBeaconManagerUUIDGroupDelegate>)delegate groupBy:(kUUIDGroupBy)groupBy;
+-(instancetype)initWithOptions:(kUUIDGroupInitOptions)initOptions delegate:(id <ESTBeaconManagerUUIDGroupDelegate>)delegate sortBy:(kUUIDGroupSortBy)sortBy;
+
+
+@end
---
+++ ESTCompanion/Categories/ESTBeaconManager+UUIDGroup.m
@@ -0,0 +1,123 @@
+//
+//  ESTBeaconManager+UUIDGroup.m
+//  estimoteTraining
+//
+//  Created by Jonathon Hibbard on 5/23/14.
+//  Copyright (c) 2014 DataOCD. All rights reserved.
+//
+
+#import <objc/runtime.h>
+#import "ESTBeaconManager+UUIDGroup.h"
+
+static char kAssociatedRegionKey;
+
+@implementation ESTBeaconManager ( UUIDGroup )
+
+-(instancetype)initWithDelegate:(id <ESTBeaconManagerUUIDGroupDelegate>)delegate {
+    return [self initWithOptions:kUUIDGroupInitOptionsNone delegate:delegate groupBy:kUUIDGroupByNone sortBy:kUUIDGroupSortByDefault];
+}
+
+-(instancetype)initWithDelegate:(id <ESTBeaconManagerUUIDGroupDelegate>)delegate groupBy:(kUUIDGroupBy)groupBy {
+    return [self initWithOptions:kUUIDGroupInitOptionsNone delegate:delegate groupBy:groupBy sortBy:kUUIDGroupSortByDefault];
+}
+
+-(instancetype)initWithDelegate:(id <ESTBeaconManagerUUIDGroupDelegate>)delegate sortBy:(kUUIDGroupSortBy)sortBy {
+    return [self initWithOptions:kUUIDGroupInitOptionsNone delegate:delegate groupBy:kUUIDGroupByNone sortBy:sortBy];
+}
+
+-(instancetype)initWithDelegate:(id <ESTBeaconManagerUUIDGroupDelegate>)delegate groupBy:(kUUIDGroupBy)groupBy sortBy:(kUUIDGroupSortBy)sortBy {
+    return [self initWithOptions:kUUIDGroupInitOptionsNone delegate:delegate groupBy:groupBy sortBy:sortBy];
+}
+
+-(instancetype)initWithOptions:(kUUIDGroupInitOptions)initOptions delegate:(id <ESTBeaconManagerUUIDGroupDelegate>)delegate {
+    return [self initWithOptions:initOptions delegate:delegate groupBy:kUUIDGroupByNone sortBy:kUUIDGroupSortByDefault];
+}
+
+-(instancetype)initWithOptions:(kUUIDGroupInitOptions)initOptions delegate:(id <ESTBeaconManagerUUIDGroupDelegate>)delegate groupBy:(kUUIDGroupBy)groupBy {
+    return [self initWithOptions:initOptions delegate:delegate groupBy:groupBy sortBy:kUUIDGroupSortByDefault];
+}
+
+-(instancetype)initWithOptions:(kUUIDGroupInitOptions)initOptions delegate:(id <ESTBeaconManagerUUIDGroupDelegate>)delegate sortBy:(kUUIDGroupSortBy)sortBy {
+    return [self initWithOptions:initOptions delegate:delegate groupBy:kUUIDGroupByNone sortBy:sortBy];
+}
+
+-(instancetype)initWithOptions:(kUUIDGroupInitOptions)initOptions delegate:(id <ESTBeaconManagerUUIDGroupDelegate>)delegate groupBy:(kUUIDGroupBy)groupBy sortBy:(kUUIDGroupSortBy)sortBy {
+
+    self = [super init];
+
+    if( self ) {
+        [self setDelegate:delegate];
+        [self processInitOptions:initOptions];
+    }
+
+    return self;
+}
+
+
+#pragma mark - Process Init Options ( kUUIDGroupInitOptions NS_OPTIONS )
+
+-(void)processInitOptions:(kUUIDGroupInitOptions)initOptions {
+
+    if( initOptions & kUUIDGroupInitOptionsNone ) {
+        return;
+    }
+
+    self.avoidUnknownStateBeacons = !!( initOptions & kUUIDGroupInitOptionsAvoidUnknownStateBeacons );
+
+    if( initOptions & kUUIDGroupInitOptionsStartRangingImmediately ) {
+        [self startRangingImmediately];
+    }
+}
+
+
+#pragma mark -
+#pragma mark StartRangingImmediately Methods
+#pragma mark -
+
+-(void)startRangingImmediately {
+
+    // Make sure that we have everything we need in order to make this work!!
+    if( ![self.delegate respondsToSelector:@selector( ESTBeaconManagerDelegateShouldReturnUUIDIdentifierArray ) ] ) {
+
+        NSLog(@"Delegate did not implement ESTBeaconManagerDelegateShouldReturnUUIDIdentifierArray - Can't Start Ranging Immediately");
+        return;
+    }
+
+    NSArray *uuidIdentifierArray = [self.delegate performSelector:@selector(ESTBeaconManagerDelegateShouldReturnUUIDIdentifierArray)];
+    if( ![self isValidUUIDIdentifierArray:uuidIdentifierArray] ) {
+        NSLog(@"Delegate did not return an array with [NSUUID, STRING] - Can't Start Ranging Immediately");
+        return;
+    }
+
+    NSString *identifier = [uuidIdentifierArray[1] copy];
+    ESTBeaconRegion *region = [[ESTBeaconRegion alloc] initWithProximityUUID:uuidIdentifierArray[0]
+                                                                  identifier: identifier];
+    [self setAssociatedRegion:region];
+
+    [self startRangingBeaconsInRegion:[self associatedRegion]];
+
+    NSLog(@"Ranging Started Immediately");
+}
+
+
+-(BOOL)isValidUUIDIdentifierArray:(NSArray *)uuidIdentifierArray {
+    return uuidIdentifierArray && [uuidIdentifierArray count] == 2 && [uuidIdentifierArray[0] isKindOfClass:[NSUUID class]] && [uuidIdentifierArray[1] isKindOfClass:[NSString class]];
+}
+
+
+#pragma mark - Yuck...
+
+/**
+ * I'm not a fan of associated objects ( note to self: write a drunken rant about it another day ).  Anyways.
+ *
+ * @todo Clearly, we need to just create a NSObject or another Category somewhere to be the entry point to this Category...For now, this will have to do...
+ */
+-(void)setAssociatedRegion:(ESTBeaconRegion *)associatedRegion {
+    objc_setAssociatedObject( self, &kAssociatedRegionKey, associatedRegion, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+-(ESTBeaconRegion *)associatedRegion {
+    return objc_getAssociatedObject( self, &kAssociatedRegionKey);
+}
+
+@end
