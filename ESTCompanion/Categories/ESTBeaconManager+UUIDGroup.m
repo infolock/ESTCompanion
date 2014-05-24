625e8d9f96353f2bdc39b4b0e0d5f21961111030
Jonathon Hibbard <jhibbard@gmail.com>
2014-05-24 02:21:43 -0400

    Update ESTBeaconManager+UUIDGroup.m

--- ESTCompanion/Categories/ESTBeaconManager+UUIDGroup.m
+++ ESTCompanion/Categories/ESTBeaconManager+UUIDGroup.m
@@ -1,6 +1,6 @@
 //
 //  ESTBeaconManager+UUIDGroup.m
-//  estimoteTraining
+//  ESTCompanion
 //
 //  Created by Jonathon Hibbard on 5/23/14.
 //  Copyright (c) 2014 Jonathon Hibbard. All rights reserved.
