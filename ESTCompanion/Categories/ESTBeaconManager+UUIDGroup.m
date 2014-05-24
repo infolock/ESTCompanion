1ee1d831db76d42316f3eea68fbe99766b826493
Jonathon Hibbard <jhibbard@gmail.com>
2014-05-24 02:15:47 -0400

    WIP

--- ESTCompanion/Categories/ESTBeaconManager+UUIDGroup.m
+++ ESTCompanion/Categories/ESTBeaconManager+UUIDGroup.m
@@ -3,7 +3,7 @@
 //  estimoteTraining
 //
 //  Created by Jonathon Hibbard on 5/23/14.
-//  Copyright (c) 2014 DataOCD. All rights reserved.
+//  Copyright (c) 2014 Jonathon Hibbard. All rights reserved.
 //
 
 // WIP!
