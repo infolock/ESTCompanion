d020eef573624e8cc88b934bb8f2a6e21e6873f3
Jonathon Hibbard <jhibbard@gmail.com>
2014-05-29 22:25:13 -0400

    Update ESTBeaconManager+AutoRegioning.m

--- ESTCompanion/ESTCompanion/Categories/ESTBeaconManager+AutoRegioning.m
+++ ESTCompanion/ESTCompanion/Categories/ESTBeaconManager+AutoRegioning.m
@@ -3,21 +3,29 @@
 //  ESTCompanion-Example
 //
 //  Created by Jonathon Hibbard on 5/29/14.
-//  Copyright (c) 2014 estcompanion. All rights reserved.
+//  Copyright (c) 2014 Jonathon Hibbard. All rights reserved.
 //
 
 #import "ESTBeaconManager+AutoRegioning.h"
 
 @implementation ESTBeaconManager (AutoRegioning)
 
--(instancetype)initWithAutoRegioningWithOptions:(kUUIDGroupInitOptions)initOptions delegate:(id <ESTBeaconManagerDelegate>)delegate groupBy:(kUUIDGroupBy)groupBy sortBy:(kUUIDGroupSortBy)sortBy proximityUUID:(NSUUID *)proximityUUID identifier:(NSString *)identifier {
+-(instancetype)initAutoRegioningWithDelegate:(id <ESTBeaconManagerDelegate>)delegate proximityUUID:(NSUUID *)proximityUUID identifier:(NSString *)identifier {
+    return [self initAutoRegioningWithOptions:kUUIDGroupInitOptionsNone delegate:delegate groupBy:kUUIDGroupByNone sortBy:kUUIDGroupSortByCLProximity proximityUUID:proximityUUID identifier:identifier];
+}
+
+-(instancetype)initAutoRegioningWithOptions:(kUUIDGroupInitOptions)initOptions delegate:(id <ESTBeaconManagerDelegate>)delegate proximityUUID:(NSUUID *)proximityUUID identifier:(NSString *)identifier {
+    return [self initAutoRegioningWithOptions:kUUIDGroupInitOptionsNone delegate:delegate groupBy:kUUIDGroupByNone sortBy:kUUIDGroupSortByCLProximity proximityUUID:proximityUUID identifier:identifier];
+}
+
+-(instancetype)initAutoRegioningWithOptions:(kUUIDGroupInitOptions)initOptions delegate:(id <ESTBeaconManagerDelegate>)delegate groupBy:(kUUIDGroupBy)groupBy sortBy:(kUUIDGroupSortBy)sortBy proximityUUID:(NSUUID *)proximityUUID identifier:(NSString *)identifier {
 
     // Ensure we aren't passing in the startRangingImmediately here as this category will take care of it instead...
     initOptions &= ~kUUIDGroupInitOptionsStartRangingImmediately;
 
     ESTBeaconManager *beaconManager = [self initWithOptions:initOptions delegate:delegate groupBy:groupBy sortBy:sortBy];
     if( beaconManager ) {
-
+        [self startRangingImmediatelyWithProximityUUID:proximityUUID identifier:identifier];
     }
 
     return beaconManager;
